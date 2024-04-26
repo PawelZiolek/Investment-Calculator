5d928a830463f043760d3852728b89394c27d330
PawelZiolek <pawel.ziolek95@gmail.com>
2024-04-26 13:01:18 +0200

    Fisrt commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import Header from "./components/Header.jsx";
+import UserInput from "./components/UserInput.jsx";
+import Results from "./components/Results.jsx";
+import { useState } from "react";
+
+function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1000,
+    expectedReturn: 4,
+    duration: 10,
+  });
+
+  const inputIsValid = userInput.duration >= 1;
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+
+  return (
+    <>
+      <Header />
+      <UserInput userInput={userInput} onChangeInput={handleChange} />
+      {!inputIsValid && (
+        <p className="center">Please enter a duration greater than 0.</p>
+      )}
+      {inputIsValid && <Results input={userInput} />}
+    </>
+  );
+}
+
+export default App;
